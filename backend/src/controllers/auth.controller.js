ef727389971f349ab5c43729e41660489e61844b
Sumit Sen <sumit.sen.cs@gmail.com>
2025-11-25 19:34:06 +0530

    adding login and logout controllers

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/auth.model.js";
 import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -7,7 +8,7 @@ export const signup = async (req, res) => {
   try {
     // Validate input
     if (!fullName ) {
-      return res.status(400).json({ message: "fullname is required" });
+      return res.status(400).json({ message: "fullName is required" });
     }
     if(!email ){
       return res.status(400).json({ message: "Email is required" });
@@ -15,6 +16,8 @@ export const signup = async (req, res) => {
     if(!password){
       return res.status(400).json({ message: "Password is required" });
     }
+    // console.log("incomming body",req.body);
+    
 
     if (password.length < 6) {
       return res.status(400).json({
@@ -53,7 +56,7 @@ export const signup = async (req, res) => {
       message: "User registered successfully",
       user: {
         _id: savedUser._id,
-        fullname: savedUser.fullname,
+        fullName: savedUser.fullName,
         email: savedUser.email,
       }
     });
@@ -63,3 +66,45 @@ export const signup = async (req, res) => {
     return res.status(500).json({ message: "Internal Server error" });
   }
 };
+export const login = async (req, res) => {
+  const {email,password}= req.body;
+  
+  try {
+    // Validate input
+    if(!email ){
+      return res.status(400).json({ message: "Email is required" });
+    }
+    if(!password){
+      return res.status(400).json({ message: "Password is required" });
+    }
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if(!user){
+      return res.status(400).json({ message: "Invalid email or password || user is not found" });
+    }
+    // Compare password
+    const isPassMatch = await bcrypt.compare(password,user.password);
+
+    if(!isPassMatch){
+      return res.status(400).json({ message: "Invalid email or password || user is not found" });
+    }
+    generateToken(user._id,res);
+    // Response
+    return res.status(200).json({
+      message: "Login successful",
+      user: {
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+      }
+    });
+
+}catch(error){
+    console.error("Error during login:", error);
+    return res.status(500).json({ message: "Internal Server error" });//500 means server side error
+}
+};
+export const logout = async (req,res)=>{
+  res.cookie("token","",{maxAge:0,httpOnly:true});
+  return res.status(200).json({message:"Logout Successful"});
+}
